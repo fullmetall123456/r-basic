e34928e003bb8aa6cb50b079ad7079d87bb81849
fullmetall123456 <87684030+fullmetall123456@users.noreply.github.com>
2021-09-06 16:29:25 -0400

    Update examen1.R

--- scripts/tema1/examen1.R
+++ scripts/tema1/examen1.R
@@ -1,18 +1,28 @@
-# Ejercicio 1
-opbasic = function (a,b) {
-  print("operación, división de cociente entero, en minutos")
-  print(paste(sprintf("%i : %i = ",a,b),a%/%b))
-  
-  print("operación división de cociente entero, en horas ")
-  print(paste(sprintf("%i : %i = ",a,b),a%/%b))
-  
-  print("Residuo o modulo de la division , en minutos")
-  print(paste(sprintf("%i : %i = ",a,b),a%%b))
-  
-  
-}
-opbasic(250000000,60)
-opbasic(4166666,60)
-opbasic(4166666,60)
+#Resultado división , en minutos
+250000000/60
+#Resultado división cociente entero, en horas"
+4166667%/%60
+
+#Resultado división , en minutos
+4166667%%60
+
+
+#Resultado divsión cociente entero, en dias"
+69444%/%24
+
+#Resultado división modulo o resto , horas"
+69444%%24
+
+# "Resultado división cociente entero, años"
+2893%/%365
+
+# "Resultado módulo de la división , dias"
+(2893%%365)-2
+
+
+
+
+
+
 
 
